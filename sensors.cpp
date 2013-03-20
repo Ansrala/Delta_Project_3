d544f6aa1694c7b63047c0c4e52cb6c08c09ebf5
xrabohrok <westin007@gmail.com>
2013-03-19 19:29:19 -0500

    updated sensors.cpp, more or less finished now

--- sensors.cpp
+++ sensors.cpp
@@ -29,56 +29,69 @@ sensor_msgs::LaserScan now;
 
 void loadLaser(const sensor_msgs::LaserScan& msg);
 
-void checkStateChange(const std_msgs::String& msg);
 
-void cleanLinesOut(p2_delta::lineList &linesOut, p2_delta::pointList &pointsOut);
+
+
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "p2_delta_sensors");
-  ros::NodeHandle nh;
-  //ros::NodeHandle s;
-  ros::NodeHandle talk1;
-  ros::NodeHandle talk2;
-  ros::NodeHandle sense;
- // ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+	ros::init(argc, argv, "p3_delta_sensors");
+	ros::NodeHandle nh;
 
-  ros::Rate loop_rate(10);
-
-//test
-ros::Publisher pub = nh.advertise<std_msgs::String>("topic_name", 5);
-std_msgs::String str;
-str.data = "hello world";
-pub.publish(str);
-
-
-
-//ros::topic::waitForMessage<nav_msgs::Odometry>(string("odom"), n,ros::Duration(30));
-    geometry_msgs::Twist msg;
-
+	ros::Rate loop_rate(10); 
+	p3a_delta::zoneCount  outBound;
   
-   ros::Subscriber info_get = sense.subscribe("scan", 500, loadLaser);
-   ros::Publisher info_pub1 = talk1.advertise<p2_delta::pointList>("worldinfoPoints", 50);
-   ros::Publisher info_pub2 = talk2.advertise<p2_delta::lineList>("worldinfoLines", 50);
+	ros::Subscriber info_get = nh.subscribe("scan", 500, loadLaser);
+	ros::Publisher info_pub1 = nh.advertise<p3a_delta::zoneCount>("zones", 50);
+	
+	int zoneSize = now.ranges.size() / 3;
+	int currentZone = 0;
+	int counts[3] = {0,0,0};
+	
 
-   
-   
+	while(ros::ok())
+	{
+		//parse points
+		for (int i = 0; i < now.ranges.size(); i++)
+		{
+			if ( i < (currentZone +1) * zoneSize)
+			{
+				currentZone = 0;
+			}
+			if ( i < (currentZone + 2) * zoneSize && i > (currentZone +1) * zoneSize )
+			{
+				currentZone = 1;
+			}
+			if (i > (currentZone + 2) * zoneSize)
+			{
+				currentZone = 3;
+			}
+			
+			//is it in an acceptable range?
+			if ( !(now.ranges[i] >.6f && now.ranges[i] < 3))
+			{
+				counts[currentZone] ++;
+			}
+			
+		}
+			
+			
+		outBound.zone1 = counts[0];
+		outBound.zone2 = counts[1];
+		outBound.zone3 = counts[2];	
 
-ROS_INFO("lines out size: %d",linesOut.x1.size());
-info_pub2.publish(linesOut);
-info_pub1.publish(pointsOut);
+		
+		info_pub1.publish(outBound);
 
 
-//ROS_INFO("ENDING LOOP Count: %d", testCount);
-//testCount++;
-ros::spinOnce();
-loop_rate.sleep();
+		ros::spinOnce();
+		loop_rate.sleep();
 
-}
+	}
 
 
 
-//end your methadology
+	//end your methadology
 
 
   return 0;
@@ -109,15 +122,6 @@ void loadLaser(const sensor_msgs::LaserScan& msg)
 
 }
 
-void cleanLinesOut(p2_delta::lineList &linesOut, p2_delta::pointList &pointsOut)
-{
-	linesOut.x1.erase(linesOut.x1.begin(), linesOut.x1.end());
-	linesOut.y1.erase(linesOut.y1.begin(), linesOut.y1.end());
-	linesOut.x2.erase(linesOut.x2.begin(), linesOut.x2.end());
-	linesOut.y2.erase(linesOut.y2.begin(), linesOut.y2.end());
 
-	pointsOut.x.erase(pointsOut.x.begin(), pointsOut.x.end());
-	pointsOut.y.erase(pointsOut.y.begin(), pointsOut.y.end());
-}
 
 
