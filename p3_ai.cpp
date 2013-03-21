2333d26290bb833ad215020d1e229e6ba65e74ba
Westin Breger <wbreger@eb2029-05.(none)>
2013-03-20 21:18:54 -0500

    ai node more or less finished (*lies*)

--- p3_ai.cpp
+++ p3_ai.cpp
@@ -1,4 +1,10 @@
 
+#define OCC_THRESHOLD .15f
+#define TRIP_THRESHOLD .3f
+#define OFFCENTER 2
+
+#define robotWidth .29f
+
 
 #include "ros/ros.h"
 #include "serializer/SensorState.h"
@@ -6,24 +12,29 @@
 #include <nav_msgs/Odometry.h>
 #include <string>
 #include <std_msgs/String.h>
+#include <cmath>
 
 //our custom messages
-
+#include <p3a_delta/zoneCount.h>
 #include <p3a_delta/command.h>
 
 using namespace std;
 
 
 serializer::SensorState serialSensors;
+p3a_delta::zoneCount zones;
 
 bool tryingLeft;
 bool tryingRight;
 bool tryingForward;
 bool tryingBackward;
 
-void getCurrCommand(const p3a_delta::command& msg);
+enum zoneStates {FREE, OCCUPIED, TRIPPED};
+
+//void getCurrCommand(const p3a_delta::command& msg);
 
 void checkSensorChange(const serializer::SensorState& msg);
+void doNothing(p3a_delta::command &msg);
 
 
 float getValue();
@@ -34,9 +45,11 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "p3_delta_ai");
   ros::NodeHandle n;
 
-  ros::Publisher cmd_vel_pub = n.advertise<p3a_delta::command>("command", 10);
+  ros::Publisher commandPub = n.advertise<p3a_delta::command>("command", 10);
 
-  geometry_msgs::Twist output;
+  p3a_delta::command outBound;
+  
+  bool escape = false;
 
 ros::Rate loop_rate(10);
 
@@ -47,6 +60,7 @@ ros::Rate loop_rate(10);
 ros::topic::waitForMessage<nav_msgs::Odometry>(std::string("odom"), n,ros::Duration(30));
     geometry_msgs::Twist msg;
 
+	ros::Subscriber subz = n.subscribe("p3a_delta/zoneCount", 10, checkSensorChange);
 
    //we listen a little faster than we publish, since we don't know when it will change
     ros::Subscriber subc = n.subscribe("serializer/sensors", 10, checkSensorChange);
@@ -57,12 +71,175 @@ ros::topic::waitForMessage<nav_msgs::Odometry>(std::string("odom"), n,ros::Durat
 //begin your methodology
 ROS_INFO("STARTING LOOP");
 
+//how many zones are we talkin'
+int zoneCount = zones.zone.size();
+int zonePointSize = zones.pointNum / zoneCount;
+float zoneAngleSize = zonePointSize * zones.angleIncrement;
+vector<zoneStates> states;
+states.resize(zoneCount);
+
+//how many zones open do we need to go forward?
+float lsquared = zones.minScanDist * zones.minScanDist;
+float angleMinOpen = acos((robotWidth*robotWidth - 2 * lsquared)/(2 * lsquared));
+int zoneMinOpen = angleMinOpen / (zonePointSize * zones.angleIncrement);
+if (zoneMinOpen == 0)
+	zoneMinOpen = 1;
+	
+float totalRange = zones.pointNum * zones.angleIncrement;
+
+int count = 0;
+bool fromRight = true;
+bool forwardOpen = true;
+int left = 0;
+int right = 0;
+bool forwardRight = true;
+bool forwardLeft = true;
+int k = 0;
+	
+
+
 while(ros::ok())
 {
 
+	doNothing(outBound);
+	//process zones
+	for ( int i = 0; i < zoneCount; i ++)
+	{
+		if( (zones.zone[i]/(float(zonePointSize))) >= TRIP_THRESHOLD)
+		{
+			states[i] = TRIPPED;
+		}
+		else if( (zones.zone[i]/(float(zonePointSize))) >= OCC_THRESHOLD)
+		{
+			states[i] = OCCUPIED;
+		}
+		else
+		{
+			states[i] = FREE;
+		}
+	}
 	
+	//rotate from the left, take the last free zone detected
+	count = 0;
+	fromRight = true;
+	for (int i = zoneCount-1; i >= 0; i--)
+	{
+		if(states[i] == FREE)
+		{
+			count++;
+		}
+		else
+			break;
+	}
 	
-
+	//the immediate right is blocked
+	if ( count == 0)
+	{
+		fromRight = false;
+		count = 0;
+		//search from right
+		for (int i = 0; i < zoneCount; i++)
+		{
+			if(states[i] == FREE)
+			{
+				count++;
+			}
+			else
+				break;
+		}
+		//shit, the right is blocked
+		if(count == 0)
+		{
+			//forward maybe?
+			forwardRight = true;
+			forwardLeft = true;
+			k = 0;
+			while ((forwardLeft || forwardRight) && (k < (zoneCount/2)))
+			{
+				//check left
+				if(states[zoneCount/2 -k] == FREE)
+				{
+					left++;
+				}
+				else
+				{
+					forwardLeft = false;
+				}
+				
+				//check right
+				if(states[zoneCount/2 +k] == FREE)
+				{
+					right++;
+				}
+				else
+				{
+					forwardRight = false;
+				}
+				k++;
+			}
+			
+			//if the two sides are big enough to pass, consider going forward
+			if((left + right) >= zoneMinOpen)
+			{
+				//see if the opening is centered
+				//it is not.
+				if( (left-right) > OFFCENTER	)
+				{
+					if (left > right)
+					{
+						outBound.turnLeft = true;
+					}
+					else
+					{
+						outBound.turnRight = true;
+					}
+				}
+				else //it is centered, and big enough
+				{
+					outBound.forward = true;
+				}
+				
+			}
+			//it simply cannot go forward
+			else
+			{
+				outBound.backUp = true;
+			}
+		}//end of forward check
+	}//end of right check
+		
+	//one of the sides was checkable
+	if (count > 0)
+	{
+		
+		if(fromRight)
+		{
+			if(zoneAngleSize * count < (totalRange/6 *2))
+			{
+				//go forward
+				outBound.forward = true;
+			}
+			else 
+			{
+				outBound.turnRight = true;
+			}
+		}
+		else if(!fromRight)
+		{
+			if(zoneAngleSize * count > (totalRange/6 * 4))
+			{
+				//go forward
+				outBound.forward = true;
+			}
+			else 
+			{
+				outBound.turnLeft = true;
+			}
+		}
+	}
+	//checkable side
+		
+	commandPub.publish(outBound);
 
 	ros::spinOnce();
 	loop_rate.sleep();
@@ -103,9 +280,21 @@ void getCurrCommand(const p3a_delta::command& msg)
 	tryingLeft = msg.turnLeft;
 	tryingRight = msg.turnRight;
 	tryingForward = msg.forward;
-	tryingBackward = msg.backward;
+	tryingBackward = msg.backUp;
 }
 
+
+void getCurrZones(const p3a_delta::zoneCount msg)
+{
+	zones.pointNum = msg.pointNum;
+	zones.angleIncrement = msg.angleIncrement;
+	
+	zones.zone.erase(zones.zone.begin(), zones.zone.end());
+	for(int i = 0; i < msg.zone.size(); i++)
+	{
+		zones.zone.push_back(msg.zone[i]);
+	}
+}
 
 float getValue()
 {
@@ -114,6 +303,13 @@ float getValue()
 	else return -1;
 }
 
+void doNothing(p3a_delta::command &msg)
+{
+	msg.turnLeft = false;
+	msg.turnRight = false;
+	msg.backUp = false;
+	msg.forward = false;
+}
 
 
 
